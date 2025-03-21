b6d9f57a112896f2c4875e3793a68148b19a39e4
Roman Sandratskiy <roman@MacBook-Pro-Roman.local>
2025-03-22 01:45:47 +0200

    Improved error handling

--- backend/MapBackend/Controllers/PlacesController.cs
+++ backend/MapBackend/Controllers/PlacesController.cs
@@ -4,15 +4,19 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.Data.Common;
 
 [ApiController]
 [Route("api/[controller]")]
 public class PlacesController : ControllerBase
 {
     private readonly AppDbContext _context;
-    public PlacesController(AppDbContext context)
+
+    private readonly ILogger<PlacesController> _logger;
+    public PlacesController(AppDbContext context, ILogger<PlacesController> logger)
     {
-        _context = context;
+      _context = context;
+      _logger = logger;
     }
 
     [HttpGet]
@@ -35,30 +39,24 @@ public class PlacesController : ControllerBase
     [HttpPost]
     public async Task<ActionResult<Place>> AddPlace([FromBody] Place newPlace)
     {
-        if(!ModelState.IsValid){
-        return BadRequest(ModelState);
-      }
-
-        newPlace.Id = 0; 
-
-        _context.Places.Add(newPlace);
-        await _context.SaveChangesAsync();
-
-        return CreatedAtAction(nameof(GetPlace), new { id = newPlace.Id }, newPlace);
-    }
-
-    [HttpDelete("{id}")]
-    public async Task<ActionResult> DeletePlace(int id)
-    {
-        var place = await _context.Places.FindAsync(id);
-        if (place == null)
+        if(!ModelState.IsValid)
         {
-            return NotFound();
+          return BadRequest(ModelState);
+        }
+        try
+        {
+          newPlace.Id = 0; 
+          _context.Places.Add(newPlace);
+          await _context.SaveChangesAsync();
+          return CreatedAtAction(nameof(GetPlace), new { id = newPlace.Id }, newPlace);
+        }
+        catch(DbUpdateException ex)
+        {
+          _logger.LogError(ex, "Error adding place to database.");
+          return StatusCode(500, "An error occurred while saving the place.");
         }
 
-        _context.Places.Remove(place);
-        await _context.SaveChangesAsync();
-        return NoContent();
+                    
     }
 
     [HttpPut("{id}")]
@@ -69,23 +67,58 @@ public class PlacesController : ControllerBase
         return BadRequest("ID in URl and in body of the request must match.");
       }
 
-      if(!ModelState.IsValid){
+      if(!ModelState.IsValid)
+      {
         return BadRequest(ModelState);
       }
-      var existingPlace = await _context.Places.FindAsync(id);
-      if (existingPlace == null)
+      try
       {
-        return NotFound();
+        var existingPlace = await _context.Places.FindAsync(id);
+        if (existingPlace == null)
+        {
+          return NotFound();
+        }
+
+        existingPlace.Name = updatedPlace.Name;
+        existingPlace.Description = updatedPlace.Description;
+        existingPlace.Latitude = updatedPlace.Latitude;
+        existingPlace.Longitude = updatedPlace.Longitude;
+
+        await _context.SaveChangesAsync();
+        return NoContent();
       }
-
-      existingPlace.Name = updatedPlace.Name;
-      existingPlace.Description = updatedPlace.Description;
-      existingPlace.Latitude = updatedPlace.Latitude;
-      existingPlace.Longitude = updatedPlace.Longitude;
-
-      await _context.SaveChangesAsync();
-      return NoContent();
+      catch(DbUpdateException ex)
+      {
+        _logger.LogError(ex, "Error updating place to database");
+        return StatusCode(500, "An error occurred while updating the place.");
+      }
+      
     }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeletePlace(int id)
+    {
+      try
+      {
+        var place = await _context.Places.FindAsync(id);
+        if (place == null)
+        {
+            return NotFound();
+        }
+
+        _context.Places.Remove(place);
+        await _context.SaveChangesAsync();
+        return NoContent();
+      }
+      catch(DbUpdateException ex)
+      {
+        _logger.LogError(ex, "Error deleting place to database");
+        return StatusCode(500, "An error occurred while deleting the place.");
+      }
+        
+    }
+
+    
 }
 
 public class Place
@@ -97,12 +130,12 @@ public class Place
     public string Name { get; set; }
 
     [Required(ErrorMessage = "Description is required.")]
-    [StringLength(300, ErrorMessage ="Description cannot exceed 300 characters.") ]
+    [StringLength(300, ErrorMessage = "Description cannot exceed 300 characters.") ]
     public string Description { get; set; }
 
     [Range(-90, 90, ErrorMessage = "Latitude must be between -90 and 90.")]
     public double Latitude { get; set; }
 
-    [Range(-180, 180, ErrorMessage ="Longitude must be -180 and 180.")]
+    [Range(-180, 180, ErrorMessage = "Longitude must be -180 and 180.")]
     public double Longitude { get; set; }
 }
